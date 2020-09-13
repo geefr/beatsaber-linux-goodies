6fea6959a5a5db95ad530ea4ca1ea6da04aab54d
Ominitay <37453713+Ominitay@users.noreply.github.com>
2020-09-13 12:17:58 +0100

    Add workarounds concerning wine bug 49532
    The initial commit to add the workarounds concerning wine bug 49532
    (https://bugs.winehq.org/show_bug.cgi?id=49532).
    The workaround script works by giving each mscorsvw.exe process a minute
    to run, and then terminating them. This delay ensure that each process
    is allowed to finish its task before being closed.
    
    This workaround should not be removed when wine bug 49532 is resolved,
    rather it should be only run on affected wine versions.

---
+++ scripts/workarounds/wine-49532-workaround.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+function testinarray {
+  local -n array=$1
+  local -n testvalue=$2
+
+  for i in "${array[@]}"
+  do
+    if [ "$i" = "$testvalue" ]
+    then
+      return 1
+    fi
+  done
+  return 0
+}
+
+oldpids=()
+
+while true
+do
+  pids=( $(pgrep mscorsvw.exe | tr "\n" " ") )
+  newpids=()
+  for pid in "${pids[@]}"
+  do
+    testinarray oldpids pid # If pid is new
+    if [ "$?" = "0" ]
+    then
+      newpids+=("$pid")
+      ./proc-stopper.sh $pid 60 &
+    fi
+  done
+  #echo "PID DEBUG BELOW"
+  #echo "All: ${pids[@]}"
+  #echo "New: ${newpids[@]}"
+  #echo "Old: ${oldpids[@]}"
+  oldpids=( ${pids[@]} )
+  sleep 1
+done
