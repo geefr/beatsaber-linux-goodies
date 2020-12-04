89c06396b7cb5bc7cd354ee94126de2c332e0e2e
Gareth Francis <gfrancis.dev@gmail.com>
2020-12-04 22:07:20 +0000

    Tidy up for initial release

--- Beataroni/build/publish-linux-x64.sh
+++ Beataroni/build/publish-linux-x64.sh
@@ -3,4 +3,5 @@ buildDir=$(dirname $0)
 dotnet publish /p:PublishProfile=${buildDir}/../Beataroni/Properties/PublishProfiles/Linux64.pubxml
 cp ${buildDir}/Beataroni-Linux.sh ${buildDir}/../Beataroni/bin/Release/net5.0/publish-Linux64/
 cp ${buildDir}/../../IPA/bin-minimal-linux64/IPA-Minimal ${buildDir}/../Beataroni/bin/Release/net5.0/publish-Linux64/
+cp ${buildDir}/../../IPA/bin-minimal-linux64/IPA-Minimal-Licence.txt ${buildDir}/../Beataroni/bin/Release/net5.0/publish-Linux64/
 
